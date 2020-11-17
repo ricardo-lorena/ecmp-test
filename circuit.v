86595d742ed74e12b78b0e62c748f13acc736553
ricardo-lorena <ricardolorena@tecnico.ulisboa.pt>
2020-11-17 22:54:36 +0000

    3pergunta

--- circuit.v
+++ circuit.v
@@ -2,16 +2,37 @@
 `include "iob_lib.vh"
 
 module circuit
+ #(
+    parameter W=32
+    )
   (
    `INPUT(rst,1),
    `INPUT(clk,1),
    `INPUT(en,1),
-   `INPUT(x,32),
-   `OUTPUT(y,32)
+   `OUTPUT(y,W)
+	
    );
 
-   `SIGNAL(acc, 32)
-   `ACC_ARE(clk, rst, 1'b0, en, acc, x)
-   `SIGNAL2OUT(y, acc)
+   `SIGNAL(cnt, 7) //7-bit counter as iterator
+   
+   `SIGNAL(y_int, W) //internal y
+   `SIGNAL(y_int_n_1, W) 
+   `SIGNAL(x, W) 
+   `SIGNAL2OUT(y, y_int) //connect internal y to output
+	
+
+   //y shift register 
+   `REG_ARE(clk, rst, 1'b0, cnt!=99, y_int_n_1, y_int)
+
+`COUNTER_ARE(clk, rst, cnt!=99, cnt)
+`COMB begin 
+if (cnt==0) y_int=x;
+else
+y_int = 2*y_int_n_1  +x; 
+
+
+end
+
+  
    
 endmodule
--- circuit_tb.v
+++ circuit_tb.v
@@ -1,13 +1,15 @@
-`timescale 1ns / 1ps
+ `timescale 1ns / 1ps
 `include "iob_lib.vh"
+
+`define W 96
 
 module circuit_tb;
 
    `CLOCK(clk, 10)
    `RESET(rst, 1, 10)
    `SIGNAL(en, 1)
-   `SIGNAL(x, 32)
-   `SIGNAL_OUT(y, 32)
+   `SIGNAL_OUT(y, `W)
+   `SIGNAL(x,90)
 
    integer i;
    
@@ -16,21 +18,25 @@ module circuit_tb;
       $dumpvars();
       en=0;
       for (i=0; i<100; i=i+1) begin
-         @(posedge clk) #1 x=i; en=1;
-         @(posedge clk) #1 en=0;
+	x=$random;
+         @(posedge clk) #1 en=1; $display("%d: %d",i+1, y);
       end
 
       @(posedge clk) #100 $finish;
 
    end 
 
-   circuit c0
+   circuit
+     #(
+       .W(`W)
+       ) 
+   c0
      (
       .rst(rst),
       .clk(clk),
       .en(en),
-      .x(x),
       .y(y)
+.x(x)
       );
 
 
